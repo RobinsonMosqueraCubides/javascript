8f337deb8f7ab957115596712f776cae9b40238e
RobinsonMosqueraCubides <robin.mosquera13@gmail.com>
2024-04-23 07:20:26 -0500

    feat: :sparkles: Dia 7
    Se corrigen errores y agrega la busqueda

--- dia7/app.js
+++ dia7/app.js
@@ -1,6 +1,8 @@
+
+
 function buscarSuperheroePorId(id) {
 
-    let url = `https://superheroapi.com/api/b263cb697abdfe978c51bbfee48142b1/${id}`;
+    let url = `https://superheroapi.com/api.php/b263cb697abdfe978c51bbfee48142b1/${id}`;
 
     fetch(url)
         .then(response => {
@@ -10,8 +12,10 @@ function buscarSuperheroePorId(id) {
             return response.json();
         })
         .then(data => {
-            console.log(url);
-            console.log(data.name);
+            let nombre = document.getElementById('name');
+            nombre.textContent = data.name;
+            let imagen = document.getElementById('img');
+            imagen.setAttribute('src', `https://superheroapi.com/api.php/b263cb697abdfe978c51bbfee48142b1/${id}/appearance`);
         })
         .catch(error => {
             console.error('Error:', error);
