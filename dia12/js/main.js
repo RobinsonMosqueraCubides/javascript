2c1ca757a461631012ede1b81d6e930ee6f4979e
RobinsonMosqueraCubides <152357795+RobinsonMosqueraCubides@users.noreply.github.com>
2024-05-01 21:56:59 -0500

    feat: :sparkles: Creacion del puntaje del player
    SE crean las funciones para aumentar el contador del jugador

--- dia12/js/main.js
+++ dia12/js/main.js
@@ -1,19 +1,48 @@
+let playerScore = 0, comScore = 0;
+function playerWin() {
+    document.getElementById('playerScore').style.color = 'green';
+    document.getElementById('playerScore').style.fontWeight = 'bold';
+    document.getElementById('playerScore').style.fontSize = '20px';
+    document.getElementById('playerScore').style.textTransform = 'uppercase';
+    document.getElementById('playerScore').style.textShadow = '0 0 10px green';
+    document.getElementById('playerScore').textContent = 'You Win';
+    document.getElementById('hit').disabled=true;
+    document.getElementById('stay').disabled=true;
+}
+function playerLoss(){
+    document.getElementById('playerScore').style.color ='red';
+    document.getElementById('playerScore').style.fontWeight = 'bold';
+    document.getElementById('playerScore').style.fontSize = '20px';
+    document.getElementById('playerScore').style.textTransform = 'uppercase';
+    document.getElementById('playerScore').style.textShadow = '0 0 10px red';
+    document.getElementById('playerScore').textContent = 'You Loss';
+    document.getElementById('hit').disabled=true;
+    document.getElementById('stay').disabled=true;
+}
+function getPlayerScore(valueCard1, valueCard2=0) {
+    playerScore += (isNaN(valueCard1) ? 10 : parseInt(valueCard1)) + (isNaN(valueCard2) ? 10 : parseInt(valueCard2));
+    document.getElementById('playerScore').textContent = playerScore;
+    if (playerScore > 21) {
+        playerLoss();
+    }
+    else if (playerScore == 21) {
+        playerWin();
+    }
+}
 function updateCom(deck){
-    let card1 = deck[0].image;
-    let card2 = deck[1].image;
-    document.getElementById('card3').src = card1;
-    document.getElementById('card4').src = card2;
+    document.getElementById('card3').src = deck[0].image;
+    document.getElementById('card4').src = deck[1].image;
 }
 function updatePlayer(deck) {
     const playerCards = document.querySelectorAll('img.playerCards');
     if (playerCards.length >= 2) {
-        console.log('hola card');
         let newCard = document.createElement('img');
         newCard.src = deck[0].image;
         newCard.alt = "";
         newCard.id = `card${playerCards.length + 1}`;
         newCard.classList.add('playerCards');
         document.querySelector('.playerContCards').appendChild(newCard);
+        getPlayerScore(deck[0].value);
     }
     else {
         let newCard = document.createElement('img');
@@ -28,10 +57,12 @@ function updatePlayer(deck) {
         newCard2.id = `card${playerCards.length + 2}`;
         newCard2.classList.add('playerCards');
         document.querySelector('.playerContCards').appendChild(newCard2);
+        getPlayerScore(deck[0].value, deck[1].value);
     }
 }
 
-function getCards(id) {
+function getCards(id, flag) {
+    if (!flag) {
     id.forEach((deckId, index) => {
         let url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`;
         fetch(url)
@@ -41,9 +72,20 @@ function getCards(id) {
                 else if (index === 1) updateCom(data.cards);
             })
             .catch(error => console.error('Hubo un error en la solicitud:', error));
-    });
+    });}
+    else{
+        id.forEach((deckId, index) => {
+            let url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`;
+            fetch(url)
+                .then(response => response.json())
+                .then(data => {
+                    if (index === 0) updateCom(data.cards);
+                })
+                .catch(error => console.error('Hubo un error en la solicitud:', error));
+        });
+    }
 }
-function getIdDeck(urls) {
+function getIdDeck(urls, flag = false) {
     let promises = urls.map(url =>
         fetch(url)
             .then(response => response.json())
@@ -51,7 +93,7 @@ function getIdDeck(urls) {
     );
 
     Promise.all(promises)
-        .then(array => getCards(array))
+        .then(array => getCards(array, flag))
         .catch(error => console.error('Hubo un error en la solicitud:', error));
 }
 document.addEventListener('DOMContentLoaded', () =>{
@@ -65,4 +107,10 @@ btnDrop.addEventListener('click', () => {
     let numRandom1 = Math.floor(Math.random() *10) + 1;
     const urls = [`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${numRandom1}`]
     getIdDeck(urls);
+});
+let btnStay = document.getElementById('stay');
+btnStay.addEventListener('click', () => {
+    let numRandom1 = Math.floor(Math.random() *10) + 1;
+    const urls = [`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=${numRandom1}`]
+    getIdDeck(urls, flag = true);
 });
