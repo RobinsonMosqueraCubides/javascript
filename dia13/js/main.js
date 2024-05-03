f73e18ab2cc8ce825d685bacc0892e3405c76ae6
RobinsonMosqueraCubides <152357795+RobinsonMosqueraCubides@users.noreply.github.com>
2024-05-03 08:43:33 -0500

    feat: :sparkles: Funcion de editar
    Se creea la funcion de editar y la de desahbilitar los inputs al cargar

--- dia13/js/main.js
+++ dia13/js/main.js
@@ -14,7 +14,24 @@ function getInformation() {
 function deleteInformation(nameHereo) {
     dataJson.forEach((data, index)=>{
         if (data.nameHero === nameHereo) {
-            delete(dataJson[0]);
+            delete(dataJson[index]);
+            console.log(dataJson);
+        }
+        else{
+            alert('Not found');
+        }
+    });
+}
+function editInformation(nameHereo) {
+    dataJson.forEach((data, index)=>{
+        if (data.nameHero === nameHereo) {
+            data.nameHero = document.getElementById('nameHereo').value;
+            data.ageActor = document.getElementById('ageActor').value;
+            data.nameActor = document.getElementById('nameActor').value;
+            data.textUbication = document.getElementById('textUbication').value;
+            data.poster = document.getElementById('poster').value;
+            data.dateUbication = document.getElementById('dateUbication').value;
+            data.productora = document.getElementById('productora').value;
             console.log(dataJson);
         }
         else{
@@ -30,3 +47,17 @@ let btnDelete = document.getElementById('delete');
 btnDelete.addEventListener('click', () => {
     deleteInformation(document.getElementById('nameHereo').value);
 });
+let btnEdit = document.getElementById('edit');
+btnEdit.addEventListener('click', () => {
+    editInformation(document.getElementById('nameHereo').value);
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('nameActor').disnabled = false;
+    document.getElementById('ageActor').disnabled = false;
+    document.getElementById('textUbication').disnabled = false;
+    document.getElementById('poster').disnabled = false;
+    document.getElementById('dateUbication').disnabled = false;
+    document.getElementById('productora').disnabled = false;
+    
+})
