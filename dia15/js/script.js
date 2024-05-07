dcb751e07d45fb86d37a531c09de85884f16d0c4
RobinsonMosqueraCubides <152357795+RobinsonMosqueraCubides@users.noreply.github.com>
2024-05-07 08:06:05 -0500

    feat: :sparkles: Se crea las funciones get y set
    Con la funcion get obtengo los datos del api y con la funcion set las guardo en una variable global para su uso previo, con los eventos click voy tomando la informacion y escribiendola

---
+++ dia15/js/script.js
@@ -0,0 +1,62 @@
+let jsonData;
+function setData(data) {
+    jsonData = data;
+    let photo = document.getElementById('picture');
+    photo.src = data.results[0].picture.large;
+    let title = document.getElementById('title');
+    title.textContent = `${data.results[0].name.first} ${data.results[0].name.last}`
+    let subtitle = document.getElementById('subTitle');
+    subtitle.textContent = 'Hi, my name is';
+    console.log(data.results[0]);
+}
+async function getData(){
+    let URL = 'https://randomuser.me/api/?results=1';
+    try {
+        const response = await fetch(URL);
+        const responseData = await response.json();
+        setData(responseData);
+    } catch (error) {
+        console.error('Error fetching data:', error);
+    }
+
+}
+let btnName = document.getElementById('name');
+btnName.addEventListener('click', ()=>{
+    setData(jsonData);
+});
+let btnEmail = document.getElementById('email');
+btnEmail.addEventListener('click', ()=>{
+    let email = document.getElementById('title');
+    let subtitle = document.getElementById('subTitle');
+    subtitle.textContent = 'My email address is';
+    email.textContent = jsonData.results[0].email;
+});
+let btnBirthday = document.getElementById('birthday');
+btnBirthday.addEventListener('click', ()=>{
+    let birthday = document.getElementById('title');
+    let subtitle = document.getElementById('subTitle');
+    subtitle.textContent = 'My birthday is';
+    birthday.textContent = jsonData.results[0].dob.date;
+});
+let btnPhone = document.getElementById('phone');
+btnPhone.addEventListener('click', () => {
+    let phone = document.getElementById('title');
+    let subtitle = document.getElementById('subTitle');
+    subtitle.textContent = 'My phone number is';
+    phone.textContent = jsonData.results[0].phone;
+});
+let btnAddress = document.getElementById('address');
+btnAddress.addEventListener('click', () => {
+    let address = document.getElementById('title');
+    let subtitle = document.getElementById('subTitle');
+    subtitle.textContent = 'My address is';
+    address.textContent = `${jsonData.results[0].location.street.number} ${jsonData.results[0].location.street.name}, ${jsonData.results[0].location.city}, ${jsonData.results[0].location.state}, ${jsonData.results[0].location.postcode}`;
+});
+let pass = document.getElementById('pass');
+pass.addEventListener('click', () => {
+    let password = document.getElementById('title');
+    let subtitle = document.getElementById('subTitle');
+    subtitle.textContent = 'My password is';
+    password.textContent = jsonData.results[0].login.password;
+});
+getData();
